f9063b07d790368358af0485b0f1f468a9ea0e85
bi- <bi-@freemail.hu>
2014-05-10 16:55:32 +0200

    alfa beta update

--- Client/AlfaBeta/AlfaBeta.cs
+++ Client/AlfaBeta/AlfaBeta.cs
@@ -11,75 +11,15 @@ namespace Client.AlfaBeta
         private PlayerType playerType;
         private int depth;
         private int expandedNodes;
+
         public AlphaBetaSearch(int depth)
         {
             this.depth = depth;
         }
 
-        public AlphaBetaSearch(): this(2)
+        public AlphaBetaSearch() : this(2)
         {
         }
-//        private readonly IGameExperimental<TState, TAction, TPlayer> _game;
-//        private int _expandedNodes;
-//
-//
-//        public AlphaBetaSearch(IGameExperimental<TState, TAction, TPlayer> game)
-//        {
-//            this._game = game;
-//        }
-//
-//        public TAction MakeDecision(TState state)
-//        {
-//            _expandedNodes = 0;
-//            TAction result = default(TAction);
-//            double resultValue = Double.NegativeInfinity;
-//            TPlayer player = _game.GetPlayer(state);
-//            foreach (TAction action in _game.GetActions(state))
-//            {
-//                double value = MinValue(_game.GetResult(state, action), player,
-//                    Double.NegativeInfinity, Double.PositiveInfinity);
-//                if (value > resultValue)
-//                {
-//                    result = action;
-//                    resultValue = value;
-//                }
-//            }
-//            return result;
-//        }
-//
-//        public double MaxValue(TState state, TPlayer player, double alpha, double beta)
-//        {
-//            _expandedNodes++;
-//            if (_game.IsTerminal(state))
-//                return _game.GetUtility(state, player);
-//            double value = Double.NegativeInfinity;
-//            foreach (TAction action in _game.GetActions(state))
-//            {
-//                value = Math.Max(value, MinValue(
-//                    _game.GetResult(state, action), player, alpha, beta));
-//                if (value >= beta)
-//                    return value;
-//                alpha = Math.Max(alpha, value);
-//            }
-//            return value;
-//        }
-//
-//        public double MinValue(TState state, TPlayer player, double alpha, double beta)
-//        {
-//            _expandedNodes++;
-//            if (_game.IsTerminal(state))
-//                return _game.GetUtility(state, player);
-//            double value = Double.PositiveInfinity;
-//            foreach (TAction action in _game.GetActions(state))
-//            {
-//                value = Math.Min(value, MaxValue(
-//                    _game.GetResult(state, action), player, alpha, beta));
-//                if (value <= alpha)
-//                    return value;
-//                beta = Math.Min(beta, value);
-//            }
-//            return value;
-//        }
 
         public string GetAIAlgorithmInfo()
         {
@@ -101,9 +41,9 @@ namespace Client.AlfaBeta
             //            TPlayer player = _game.GetPlayer(state);
             foreach (AbstractStep step in state.GetAvailableSteps())
             {
-                double value = 0; // TODO remove this and use below when implemented.
-                //Double value = MinValue(_game.GetResult(state, step), player,
-                                //Double.NegativeInfinity, Double.PositiveInfinity);
+                Double value = 0;
+                //Double value = MinValue(game.GetResult(state, step), playerType,
+                //                Double.NegativeInfinity, Double.PositiveInfinity);
                 if (value > resultValue)
                 {
                     result = step;
@@ -111,6 +51,40 @@ namespace Client.AlfaBeta
                 }
             }
             return result;
+        }
+
+        public double MinValue(IState state, PlayerType player, double alpha, double beta)
+        {
+            expandedNodes++;
+//            if (_game.IsTerminal(state))
+//                return _game.GetUtility(state, player);
+            double value = Double.PositiveInfinity;
+            foreach (AbstractStep step in state.GetAvailableSteps())
+            {
+                //value = Math.Min(value, MaxValue(
+                //game.GetResult(state, step), player, alpha, beta));
+                if (value <= alpha)
+                    return value;
+                beta = Math.Min(beta, value);
+            }
+            return value;
+        }
+
+        public double MaxValue(IState state, PlayerType player, double alpha, double beta)
+        {
+            expandedNodes++;
+            //     if (_game.IsTerminal(state))
+            //         return _game.GetUtility(state, player);
+            double value = Double.NegativeInfinity;
+            foreach (AbstractStep step in state.GetAvailableSteps())
+            {
+                // value = Math.Max(value, MinValue(
+                //      _game.GetResult(state, action), player, alpha, beta));
+                if (value >= beta)
+                    return value;
+                alpha = Math.Max(alpha, value);
+            }
+            return value;
         }
 
         public void AddToGame(AbstractGame game, PlayerType playerType)
