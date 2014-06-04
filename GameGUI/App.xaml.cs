10c1b8ddbe0e8cc40a653c05d949375b4891ee80
dclaudi <darcsiclaudia@gmail.com>
2014-06-04 13:21:22 +0200

    view modell chess

---
+++ GameGUI/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using GameGUI.View;
+namespace GameGUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow();
+
+            window.Show();
+        }
+    }
+}
