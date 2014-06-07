a82b1f82209a1d187b79a533c6956adf8e7c392c
Balassa Imre <balassai@gmail.com>
2014-06-07 19:57:37 +0200

    formatting

--- Client/AlfaBeta/AlfaBeta.cs
+++ Client/AlfaBeta/AlfaBeta.cs
@@ -1,19 +1,19 @@
 ﻿using System;
+using System.Linq;
 using Client.AIAlgorithmBase;
 using Game.GameBase;
-using System.Linq;
 
 namespace Client.AlfaBeta
 {
     public class AlphaBetaSearch : IAIAlgorithm
     {
-        private AbstractGame game;
-        private PlayerType playerType;
-        private int depth;
+        private readonly int _depth;
+        private AbstractGame _game;
+        private PlayerType _playerType;
 
         public AlphaBetaSearch(int depth)
         {
-            this.depth = depth;
+            _depth = depth;
         }
 
         public AlphaBetaSearch() : this(2)
@@ -25,23 +25,31 @@ namespace Client.AlfaBeta
             return "AlfaBeta AI Algorithm implementation.";
         }
 
+        public void AddToGame(AbstractGame game, PlayerType playerType)
+        {
+            _game = game;
+            _playerType = playerType;
+            AbstractGame.StepHandler stepHandler = AlfaBetaStepHandler;
+            game.RegisterAsPlayer(ref stepHandler, playerType);
+        }
+
         public void AlfaBetaStepHandler(IState state)
         {
             Console.WriteLine("AlfaBeta Stephandler has been called.");
-            if (playerType != state.CurrentPlayer) return;
+            if (_playerType != state.CurrentPlayer) return;
             AbstractStep aStep = MakeDecision(state);
-            game.DoStep(aStep, playerType);
+            _game.DoStep(aStep, _playerType);
         }
 
         public AbstractStep MakeDecision(IState state)
         {
-            int level = 0;
+            const int level = 0;
             AbstractStep result = default(AbstractStep);
             double resultValue = Double.NegativeInfinity;
-            foreach (AbstractStep step in game.GetAvailableSteps(state))
+            foreach (AbstractStep step in _game.GetAvailableSteps(state))
             {
-                Double value = MinValue(level, game.SimulateStep(state, step),
-                                Double.NegativeInfinity, Double.PositiveInfinity);
+                Double value = MinValue(level, _game.SimulateStep(state, step),
+                    Double.NegativeInfinity, Double.PositiveInfinity);
                 if (value > resultValue)
                 {
                     result = step;
@@ -51,16 +59,16 @@ namespace Client.AlfaBeta
             return result;
         }
 
-        protected double MinValue(int level, IState state, double alpha, double beta)
+        private double MinValue(int level, IState state, double alpha, double beta)
         {
             level++;
-            if (level == depth || game.GetAvailableSteps(state).ToList().Count == 0)
-                return game.GetHeuristicValue(state, playerType);
+            if (level == _depth || _game.GetAvailableSteps(state).ToList().Count == 0)
+                return _game.GetHeuristicValue(state, _playerType);
             double value = Double.PositiveInfinity;
-            foreach (AbstractStep step in game.GetAvailableSteps(state))
+            foreach (AbstractStep step in _game.GetAvailableSteps(state))
             {
                 value = Math.Min(value, MaxValue(level,
-                    game.SimulateStep(state, step), alpha, beta));
+                    _game.SimulateStep(state, step), alpha, beta));
                 if (value <= alpha)
                     return value;
                 beta = Math.Min(beta, value);
@@ -69,31 +77,21 @@ namespace Client.AlfaBeta
         }
 
 
-        protected double MaxValue(int level, IState state, double alpha, double beta)
+        private double MaxValue(int level, IState state, double alpha, double beta)
         {
             level++;
-            if (level == depth || game.GetAvailableSteps(state).ToList().Count == 0)
-                return game.GetHeuristicValue(state, playerType);
+            if (level == _depth || _game.GetAvailableSteps(state).ToList().Count == 0)
+                return _game.GetHeuristicValue(state, _playerType);
             double value = Double.NegativeInfinity;
-            foreach (AbstractStep step in game.GetAvailableSteps(state))
+            foreach (AbstractStep step in _game.GetAvailableSteps(state))
             {
                 value = Math.Max(value, MinValue(level,
-                      game.SimulateStep(state, step), alpha, beta));
+                    _game.SimulateStep(state, step), alpha, beta));
                 if (value >= beta)
                     return value;
                 alpha = Math.Max(alpha, value);
             }
             return value;
         }
-
-        public void AddToGame(AbstractGame game, PlayerType playerType)
-        {
-            this.game = game;
-            this.playerType = playerType;
-            AbstractGame.StepHandler stepHandler = AlfaBetaStepHandler;
-            game.RegisterAsPlayer(ref stepHandler, playerType);
-        }
-
     }
-  
 }
--- UnitTest/UnitTestAIAlgorithm.cs
+++ UnitTest/UnitTestAIAlgorithm.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Client.AIAlgorithmBase;
 using Client.AlfaBeta;
 using Client.MinMax;
 using Game.GameBase;
@@ -8,7 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestAIAlgorithm
 {
-    class TestGame : AbstractGame
+    internal class TestGame : AbstractGame
     {
         public override string GetGameTypeInfo()
         {
@@ -28,7 +27,7 @@ namespace UnitTestAIAlgorithm
         {
             if (state is TestState)
             {
-                return ((TestState)state).GetHValue();
+                return ((TestState) state).GetHValue();
             }
             throw new NotImplementedException();
         }
@@ -37,34 +36,36 @@ namespace UnitTestAIAlgorithm
         {
             if (current is TestState && step is TestStep)
             {
-                return ((TestState)current).getChildAt(((TestStep)step).GetStep());
+                return ((TestState) current).getChildAt(((TestStep) step).GetStep());
             }
             throw new NotImplementedException();
         }
 
         public override IEnumerable<AbstractStep> GetAvailableSteps(IState state)
         {
-            List<AbstractStep> steps = new List<AbstractStep>();
-            if (state is TestState) {
-                TestState ts = (TestState) state;
+            var steps = new List<AbstractStep>();
+            if (state is TestState)
+            {
+                var ts = (TestState) state;
                 for (int i = 0; i < ts.GetChildCount(); i++)
                 {
                     IState child = ts.getChildAt(i);
                     if (child is TestState)
                     {
-                        TestState ch = (TestState) child;
-                        steps.Add(new TestStep(child,i));
+                        var ch = (TestState) child;
+                        steps.Add(new TestStep(child, i));
                     }
                 }
             }
             return steps;
-
         }
     }
-    class TestStep : AbstractStep
+
+    internal class TestStep : AbstractStep
     {
-        private IState state;
-        private int step;
+        private readonly IState state;
+        private readonly int step;
+
         public TestStep(IState st, int i)
         {
             step = i;
@@ -82,19 +83,24 @@ namespace UnitTestAIAlgorithm
             return state;
         }
     }
-    class TestState : IState
+
+    internal class TestState : IState
     {
+        private readonly List<IState> list = new List<IState>();
         private double hValue;
-        List<IState> list = new List<IState>();
 
         public TestState(double value)
         {
             hValue = value;
         }
+
+        public PlayerType CurrentPlayer { get; set; }
+
         public void AddChild(IState state)
         {
             list.Add(state);
         }
+
         public void AddChilds(params IState[] state)
         {
             list.AddRange(state);
@@ -119,16 +125,15 @@ namespace UnitTestAIAlgorithm
         {
             return list.Count;
         }
-
-        public PlayerType CurrentPlayer { get; set; }
     }
+
     [TestClass]
     public class UnitTestAB
     {
         [TestMethod]
         public void TestMethodLevel0()
         {
-            AlphaBetaSearch abs = new AlphaBetaSearch();
+            var abs = new AlphaBetaSearch();
             IState state = new TestState(0);
             abs.AddToGame(new TestGame(), PlayerType.PlayerOne);
             AbstractStep step = abs.MakeDecision(state);
@@ -138,41 +143,39 @@ namespace UnitTestAIAlgorithm
         [TestMethod]
         public void TestMethodLevel1()
         {
-            AlphaBetaSearch abs = new AlphaBetaSearch(2);
-            TestState state = new TestState(1);
-            TestState left = new TestState(4);
-            TestState right = new TestState(3);
+            var abs = new AlphaBetaSearch(2);
+            var state = new TestState(1);
+            var left = new TestState(4);
+            var right = new TestState(3);
             state.AddChilds(left, right);
 
             abs.AddToGame(new TestGame(), PlayerType.PlayerOne);
             AbstractStep step = abs.MakeDecision(state);
-            TestState result = (TestState)((TestStep) step).GetState();
+            var result = (TestState) ((TestStep) step).GetState();
             Assert.AreEqual(4, result.GetHValue());
             Assert.AreEqual(left, result);
-
         }
 
         [TestMethod]
         public void TestMethodLevel2()
         {
-            AlphaBetaSearch abs = new AlphaBetaSearch(4);
+            var abs = new AlphaBetaSearch(4);
 
-            TestState state = new TestState(0);
-            TestState level1_0 = new TestState(0);
-            TestState level1_1 = new TestState(0);
-            TestState level2_0 = new TestState(2);
-            TestState level2_1 = new TestState(7);
-            TestState level2_2 = new TestState(1);
-            TestState level2_3 = new TestState(8);
+            var state = new TestState(0);
+            var level1_0 = new TestState(0);
+            var level1_1 = new TestState(0);
+            var level2_0 = new TestState(2);
+            var level2_1 = new TestState(7);
+            var level2_2 = new TestState(1);
+            var level2_3 = new TestState(8);
             state.AddChilds(level1_0, level1_1);
             level1_0.AddChilds(level2_0, level2_1);
             level1_1.AddChilds(level2_2, level2_3);
 
             abs.AddToGame(new TestGame(), PlayerType.PlayerOne);
             AbstractStep step = abs.MakeDecision(state);
-            TestState result = (TestState)((TestStep)step).GetState();
+            var result = (TestState) ((TestStep) step).GetState();
             Assert.AreEqual(level1_0, result);
-
         }
 
         [TestMethod]
@@ -196,8 +199,8 @@ namespace UnitTestAIAlgorithm
 
             minmax.AddToGame(new TestGame(), PlayerType.PlayerOne);
             AbstractStep step = minmax.MakeDecision(state);
-            Assert.AreEqual(1, ((TestStep)step).GetStep());
-            Assert.AreEqual(right, ((TestStep)step).GetState());
+            Assert.AreEqual(1, ((TestStep) step).GetStep());
+            Assert.AreEqual(right, ((TestStep) step).GetState());
         }
 
         [TestMethod]
@@ -217,7 +220,7 @@ namespace UnitTestAIAlgorithm
 
             minmax.AddToGame(new TestGame(), PlayerType.PlayerOne);
             AbstractStep step = minmax.MakeDecision(state);
-            Assert.AreEqual(level1_0, ((TestStep)step).GetState());
+            Assert.AreEqual(level1_0, ((TestStep) step).GetState());
         }
     }
 }
